f083f46e5337863cf928c59cea6f20f3ac7acb50
Shiyu Wang <shiyu3169@gmail.com>
2019-02-06 16:11:52 -0500

    build

---
+++ build/precache-manifest.85d898e435c600c5f5c032bfb3b9d179.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1919320d922a6827fc9b4b641438f59f",
+    "url": "/static/media/ulem.1919320d.png"
+  },
+  {
+    "revision": "0eb2c0f6418342f272af4b392929696b",
+    "url": "/static/media/logo.0eb2c0f6.PNG"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "29888760c37d3332ba83",
+    "url": "/static/js/main.29888760.chunk.js"
+  },
+  {
+    "revision": "b8c30aec9e6d9cd16fc8",
+    "url": "/static/js/1.b8c30aec.chunk.js"
+  },
+  {
+    "revision": "29888760c37d3332ba83",
+    "url": "/static/css/main.a485a9d5.chunk.css"
+  },
+  {
+    "revision": "b61817e211d28dc7fbd2e53705693e9c",
+    "url": "/index.html"
+  }
+];
