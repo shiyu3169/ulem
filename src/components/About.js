cb8efbece42d0a8011bd110598c3d5f8b7938ec7
Shiyu Wang <shiyu3169@gmail.com>
2019-02-07 12:45:49 -0500

    about

--- src/components/About.js
+++ src/components/About.js
@@ -452,6 +452,259 @@ export default class About extends Component {
                 </li>
               </ul>
             </section>
+            <section id="leadership">
+              <h1 className="font-red mt-4">Leadership</h1>
+              <p>Darnell L Williams</p>
+              <p>
+                A well-respected community leader, Darnell Williams is President
+                and CEO of the Urban League of Eastern Massachusetts, a
+                nonprofit organization providing services and programs in
+                education, career and personal development and employment for
+                African-Americans and other residents of color. Mr. Williams is
+                well-known to and often assists other culturally diverse
+                organizations beyond African Americans. Prior to joining the
+                Urban League, Mr. Williams was Manager of Management Recruitment
+                and Development at Massachusetts General Hospital (MGH) as well
+                as a Diversity Consultant in private practice.
+              </p>
+              <p>
+                Mr. Williams was President of the Springfield Branch of the
+                National Association for the Advancement of Colored People
+                (NAACP) and served as the President of the NAACP New England
+                Area Conference from 1996 to 2001. He is the former Director for
+                the United Way of Pioneer Valley. Mr. Williams served as trustee
+                of Bunker Hill Community College and Beaver Country Day School.
+                He is a former board member of the Boston Workforce Development
+                Coalition. Mr. Williams is a former member of the Boston Study
+                Group, Chief Executives’ Club of Boston, trustee of the Dana
+                Farber Cancer Institute, and the Federal Reserve Community
+                Development Advisory Board. Congressman Richard Neal then Mayor
+                of Springfield appointed Mr. Williams to serve as Vice-Chairman
+                of the Board of Fire Commissioners. Governor Michael Dukakis
+                appointed Mr. Williams to serve as Vice-Chairman within the
+                Division of Insurance for the Auto Damage Licensing Board.
+              </p>
+              <p>
+                In February 2008, he was appointed by Governor Deval Patrick to
+                the Board of Directors of the Massachusetts Bay Transit
+                Authority (MBTA). Mr. Williams was elected to the Board of
+                Directors of the Co-Operative Central Bank Board of Directors
+                and in January 2009 he was appointed to the Massachusetts Bay
+                Transportation Authority (MBTA) Retirement Fund Board of
+                Trustees. In January 2013, he was elected to the position of
+                President of the Association of Executives of the National Urban
+                League (AOE) and also appointed as a trustee to the National
+                Urban League Board of Directors.
+              </p>
+              <p>
+                A highly respected advocate and requested speaker on issues of
+                importance pertaining to the challenges faced by communities of
+                color and low-income families, Mr. Williams was a contributing
+                member to Boston’s successful effort to win the 2004 Democratic
+                National Convention. For his contributions within the Black
+                community and the City of Boston, Mayor Thomas Menino presented
+                him with the 2003 Community Service Award. Mr. Williams received
+                the 2005 Paul Parks Veteran’s Community Service Award. Mayor
+                Thomas Menino appointed him as Chairman of the Roxbury Strategic
+                Master Plan Oversight Committee (RSMPOC) in 2005. In 2007, Mr.
+                Williams was inducted into the Human Resources Alliance for
+                African Americans (HRAAA) Hall of Fame. In 2011, the Urban
+                League of Eastern Massachusetts hosted the National Urban
+                League’s Annual Conference in Boston after a 35 year absence.
+                This event has paved the way for additional conferences of color
+                to come to Boston as their host city.
+              </p>
+              <p>
+                A Gary, Indiana native, Mr. Williams earned his Master’s degree
+                in Education from Boston University after completing his
+                undergraduate degree at American International College in
+                Springfield, MA.
+              </p>
+              <p>
+                Darnell Williams and his wife Colleen, reside in Roxbury with
+                their daughter, Imani.
+              </p>
+            </section>
+            <section id="board">
+              <h1 className="font-red mt-4">Board of Directors</h1>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>BOARD</th>
+                    <th>Sal</th>
+                    <th>First</th>
+                    <th>M</th>
+                    <th>Last</th>
+                    <th>Job Title</th>
+                    <th>Organization</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Mr. </td>
+                    <td>Donnie</td>
+                    <td>&nbsp;</td>
+                    <td>Bedney</td>
+                    <td>Regional Director</td>
+                    <td>Press Ganey</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Mr. </td>
+                    <td>Derek</td>
+                    <td>E.</td>
+                    <td>Brooks </td>
+                    <td>Lead, GBR Market Development</td>
+                    <td>Comcast</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>Mr.</td>
+                    <td>Gary</td>
+                    <td>&nbsp;</td>
+                    <td>Carroway</td>
+                    <td>Senior Executive</td>
+                    <td>UPS</td>
+                  </tr>
+                  <tr>
+                    <td>4</td>
+                    <td>Ms.</td>
+                    <td>Petrina</td>
+                    <td>&nbsp;</td>
+                    <td>Cherry</td>
+                    <td>Vice President</td>
+                    <td>BMC Healthnet Plan/Well Sense Health Plan</td>
+                  </tr>
+                  <tr>
+                    <td>5</td>
+                    <td>Ms.</td>
+                    <td>Brenda</td>
+                    <td>&nbsp;</td>
+                    <td>Cole</td>
+                    <td>Vice President</td>
+                    <td>Harvard Pilgrim Health Care</td>
+                  </tr>
+                  <tr>
+                    <td>6</td>
+                    <td>Mr.</td>
+                    <td>Steve</td>
+                    <td>&nbsp;</td>
+                    <td>Duncan</td>
+                    <td>Senior Vice President</td>
+                    <td>State Street Corporation</td>
+                  </tr>
+                  <tr>
+                    <td>7</td>
+                    <td>Mr.</td>
+                    <td>Joseph</td>
+                    <td>&nbsp;</td>
+                    <td>Feaster Jr.</td>
+                    <td>Counselor at Law</td>
+                    <td>Feaster Enterprises, c/o McKenzie & Assoc., P.C.</td>
+                  </tr>
+                  <tr>
+                    <td>8</td>
+                    <td>Mr. </td>
+                    <td>Johnny</td>
+                    <td>&nbsp;</td>
+                    <td>Green</td>
+                    <td>News Director</td>
+                    <td>CBS-TV4</td>
+                  </tr>
+                  <tr>
+                    <td>9</td>
+                    <td>Ms.</td>
+                    <td>Marian</td>
+                    <td>A.</td>
+                    <td>Green-Robinson</td>
+                    <td>Branch Manager, Bank Officer</td>
+                    <td>Eastern Bank</td>
+                  </tr>
+                  <tr>
+                    <td>10</td>
+                    <td>Ms.</td>
+                    <td>Dorothea</td>
+                    <td>&nbsp;</td>
+                    <td>Jones</td>
+                    <td>Guild -National</td>
+                    <td>Urban League Guild</td>
+                  </tr>
+                  <tr>
+                    <td>11</td>
+                    <td>Ms. </td>
+                    <td>Andrea</td>
+                    <td>&nbsp;</td>
+                    <td>Kershaw</td>
+                    <td>Regional Vice President</td>
+                    <td>Enterprise Holdings</td>
+                  </tr>
+                  <tr>
+                    <td>12</td>
+                    <td>Ms.</td>
+                    <td>Deloris </td>
+                    <td>&nbsp;</td>
+                    <td>Pettis</td>
+                    <td>Risk & Audit Consultant</td>
+                    <td>Consultant</td>
+                  </tr>
+                  <tr>
+                    <td>13</td>
+                    <td>Mr.</td>
+                    <td>Malik</td>
+                    <td>A.</td>
+                    <td>Smith</td>
+                    <td>Wealth Advisor</td>
+                    <td>Alex Brown</td>
+                  </tr>
+                  <tr>
+                    <td>14</td>
+                    <td>Mr. </td>
+                    <td>Charles</td>
+                    <td>D.</td>
+                    <td>Stewart</td>
+                    <td>President & Owner</td>
+                    <td>M & S Communications Group</td>
+                  </tr>
+                  <tr>
+                    <td>15</td>
+                    <td>Ms.</td>
+                    <td>Rachelle</td>
+                    <td>&nbsp;</td>
+                    <td>Villarson</td>
+                    <td>&nbsp;</td>
+                    <td>Suffolk County Sheriff's Department</td>
+                  </tr>
+                  <tr>
+                    <td>16</td>
+                    <td>Mr.</td>
+                    <td>Mark</td>
+                    <td>&nbsp;</td>
+                    <td>Walker</td>
+                    <td>Sr. VP, Credit Product Manager</td>
+                    <td>Citizens Bank</td>
+                  </tr>
+                  <tr>
+                    <td>17</td>
+                    <td>Mr.</td>
+                    <td>Marques</td>
+                    <td>A.</td>
+                    <td>Williams</td>
+                    <td>Sr. Mgr, Business Development</td>
+                    <td>Aramark</td>
+                  </tr>
+                  <tr>
+                    <td>18</td>
+                    <td>Ms.</td>
+                    <td>Ericka</td>
+                    <td>&nbsp;</td>
+                    <td>Wilson-Kerr</td>
+                    <td>Regional Sales Director</td>
+                    <td>CVS</td>
+                  </tr>
+                </tbody>
+              </table>
+            </section>
           </main>
         </div>
       </div>
