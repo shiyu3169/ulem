06fff294a514fc5ba0173c3881a789309c7225fa
Shiyu Wang <shiyu3169@gmail.com>
2019-02-08 14:01:18 -0500

    donate

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -8,18 +8,14 @@ export default class Donate extends Component {
       <div className="container">
         <h1 className="font-red mt-4">Donate</h1>
         <p>
-          <strong>
-            ULEM operates by the grace of a combination of grants, contracts,
-            and donations. Every dollar donated to the Urban League of Eastern
-            Massachusetts is used to support programs, overhead, and salaries.
-            In order to run this agency, ULEM must raise at least $2.5 million
-            annually. That is how we provide workforce development training to
-            the under- and unemployed at no cost to them.
-          </strong>
+          ULEM operates by the grace of a combination of grants, contracts, and
+          donations. Every dollar donated to the Urban League of Eastern
+          Massachusetts is used to support programs, overhead, and salaries. In
+          order to run this agency, ULEM must raise at least $2.5 million
+          annually. That is how we provide workforce development training to the
+          under- and unemployed at no cost to them.
         </p>
-        <p>
-          <strong>lease take a moment to view our impact video:​</strong>
-        </p>
+        <p>lease take a moment to view our impact video:​</p>
         <div class="embed-responsive embed-responsive-16by9">
           <iframe
             title="donate"
@@ -29,26 +25,20 @@ export default class Donate extends Component {
           />
         </div>
         <p>
-          <strong>
-            After watching it, you may be inclined to make a donation by
-            clicking the image below:​
-          </strong>
+          After watching it, you may be inclined to make a donation by clicking
+          the image below:​
         </p>
         <a href="https://interland3.donorperfect.net/weblink/weblink.aspx?name=ulem&id=1">
           <img className="center img-fluid" src={Donating} alt="donate" />
         </a>
         <p>
-          <strong>
-            In addition to donations of any amount, ULEM also accepts planned
-            gifts and company stocks. Please note that if you give us a gift,
-            your company may also offer an employee match. That’s a great way to
-            double your gift! Please contact the development department at
-            development@ulem.org or 617.442.4519 x235 to get more information. ​
-          </strong>
+          In addition to donations of any amount, ULEM also accepts planned
+          gifts and company stocks. Please note that if you give us a gift, your
+          company may also offer an employee match. That’s a great way to double
+          your gift! Please contact the development department at
+          development@ulem.org or 617.442.4519 x235 to get more information. ​
         </p>
-        <p>
-          <strong>Thank you in advance! ​</strong>
-        </p>
+        <p>Thank you in advance!</p>
         <a href="https://smile.amazon.com/ch/23-7349132">
           <img className="center img-fluid" src={Amazon} alt="amazon" />
         </a>
