4455c4593c8a2be56263d46b5d8c3ce8c6a5b59a
Shiyu Wang <shiyu3169@gmail.com>
2019-02-22 12:09:16 -0500

    fix slides by id

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,17 +14,6 @@ export default class Home extends Component {
 
   componentDidMount() {
     this.getTopEvents();
-    /* curator-feed */
-    let i,
-      e,
-      d = document,
-      s = "script";
-    i = d.createElement("script");
-    i.async = 1;
-    i.src =
-      "https://cdn.curator.io/published/69fcc34b-8235-482b-8bd6-bb0cf90b45b4.js";
-    e = d.getElementsByTagName(s)[0];
-    e.parentNode.insertBefore(i, e);
   }
 
   getTopEvents = async () => {
--- src/components/Home/Instagram.js
+++ src/components/Home/Instagram.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 
 export default class Instagram extends Component {
+  componentDidMount() {
+    /* curator-feed */
+    let i,
+      e,
+      d = document,
+      s = "script";
+    i = d.createElement("script");
+    i.async = 1;
+    i.src =
+      "https://cdn.curator.io/published/69fcc34b-8235-482b-8bd6-bb0cf90b45b4.js";
+    e = d.getElementsByTagName(s)[0];
+    e.parentNode.insertBefore(i, e);
+  }
   render() {
     return (
       <div className="mr-5">
