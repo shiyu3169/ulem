812676397dbd153f00c2da584c092e91a30d169d
Shiyu Wang <shiyu3169@gmail.com>
2019-02-26 09:13:15 -0500

    banner

--- src/components/marathon/Marathon.js
+++ src/components/marathon/Marathon.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import MarathonNav from "./MarathonNav";
 import MarathonAbout from "./MarathonAbout";
+import MarathonBanner from "../../img/ULEM-MarathonBanner.png";
 export default class Marathon extends Component {
   render() {
     return (
       <div>
         <MarathonNav />
         <img
-          src="http://www.msdacademy.org/wp-content/uploads/2018/04/Boston-marathon-web-banner.jpg"
+          src={MarathonBanner}
           alt="Marathon"
           className="img-fluid"
           width="100%"
