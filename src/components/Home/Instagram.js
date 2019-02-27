130c548ab2a3c2f2cae81a34b157bc697b5cad48
Shiyu Wang <shiyu3169@gmail.com>
2019-02-27 15:23:42 -0500

    update instagram addon

--- src/components/Home/Instagram.js
+++ src/components/Home/Instagram.js
@@ -1,22 +1,9 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 export default class Instagram extends Component {
-  componentDidMount() {
-    /* curator-feed */
-    let i,
-      e,
-      d = document,
-      s = "script";
-    i = d.createElement("script");
-    i.async = 1;
-    i.src =
-      "https://cdn.curator.io/published/69fcc34b-8235-482b-8bd6-bb0cf90b45b4.js";
-    e = d.getElementsByTagName(s)[0];
-    e.parentNode.insertBefore(i, e);
-  }
   render() {
     return (
-      <div className="mr-5">
+      <Fragment>
         <a
           className="link"
           href="https://www.instagram.com/theulem/"
@@ -28,8 +15,15 @@ export default class Instagram extends Component {
             <i className="fab fa-instagram fa-lg float-right" />
           </h3>
         </a>
-        <div id="curator-feed" />
-      </div>
+        <iframe
+          src="//lightwidget.com/widgets/b5fa1f3fa9ca5bfbb0000608b4fa4962.html"
+          scrolling="no"
+          allowtransparency="true"
+          className="lightwidget-widget border"
+          title="instagram"
+          style={{ width: "100%", border: 0, overflow: "hidden" }}
+        />
+      </Fragment>
     );
   }
 }
