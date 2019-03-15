77d8c8811fbc743f2670b9ee44c0101716ef68eb
Shiyu Wang <shiyu3169@gmail.com>
2019-03-15 13:35:10 -0400

    layout of centennial

--- src/components/Centennial.js
+++ src/components/Centennial.js
@@ -11,7 +11,6 @@ export default class Centennial extends Component {
           className="img-fluid cursor center"
         />
         <Milestone />
-        <h2>More Info is Coming Soon</h2>
       </div>
     );
   }
---
+++ src/components/centennial/CentennialMembership.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+export default class CentennialMembership extends Component {
+  render() {
+    return (
+      <div>
+          <h1>100TH ANNIVERSARY CELEBRATION AND BENEFIT CONCERT</h1>
+      </div>
+    )
+  }
+}
