806dd0aa1c3f2d2c591fb6839c7d9262274d48df
Shiyu Wang <shiyu3169@gmail.com>
2019-03-15 11:15:35 -0400

    part of membership

---
+++ src/components/membership/MembershipCard.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export default class MembershipCard extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        <header className="ulem-bg-primary py-1">{this.props.header}</header>
+        <div className="ulem-bg-dark">
+            <p className="pt-3 m-0"><strong>{this.props.title}</strong></p>
+            <div>$<span className="card-number-lg">{this.props.price}</span>{this.props.year}</div>
+            <p><strong>{this.props.renewal}</strong></p>
+            <ul className="font-tiny p-2 mb-0">
+                <li>{this.props.li1}</li>
+                <li>{this.props.li2}</li>
+                <li>{this.props.li3}</li>
+                <li>{this.props.li4}</li>
+                <li>{this.props.li5}</li>
+                <li>{this.props.li6}</li>
+                <li>{this.props.li7}</li>
+            </ul>
+            <button className="btn btn-ulem-primary btn-round mb-3">Purchase</button>
+        </div>
+      </div>
+    )
+  }
+}
