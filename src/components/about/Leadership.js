ff60a4b49efa3ddd2cbca0ad385cabc140e59367
Shiyu Wang <shiyu3169@gmail.com>
2019-03-19 13:56:21 -0400

    about pages

--- src/components/about/Leadership.js
+++ src/components/about/Leadership.js
@@ -1,74 +1,96 @@
 import React from "react";
-
+import darnel from "../../img/Darnell.jpg";
 export default function Leadership() {
   return (
-    <section id="leadership">
-      <h1 className="font-red mt-4">Leadership</h1>
-      <p>Darnell L Williams</p>
-      <p>
-        A well-respected community leader, Darnell Williams is President and CEO
-        of the Urban League of Eastern Massachusetts, a nonprofit organization
-        providing services and programs in education, career and personal
-        development and employment for African-Americans and other residents of
-        color. Mr. Williams is well-known to and often assists other culturally
-        diverse organizations beyond African Americans. Prior to joining the
-        Urban League, Mr. Williams was Manager of Management Recruitment and
-        Development at Massachusetts General Hospital (MGH) as well as a
-        Diversity Consultant in private practice.
-      </p>
-      <p>
-        Mr. Williams was President of the Springfield Branch of the National
-        Association for the Advancement of Colored People (NAACP) and served as
-        the President of the NAACP New England Area Conference from 1996 to
-        2001. He is the former Director for the United Way of Pioneer Valley.
-        Mr. Williams served as trustee of Bunker Hill Community College and
-        Beaver Country Day School. He is a former board member of the Boston
-        Workforce Development Coalition. Mr. Williams is a former member of the
-        Boston Study Group, Chief Executives’ Club of Boston, trustee of the
-        Dana Farber Cancer Institute, and the Federal Reserve Community
-        Development Advisory Board. Congressman Richard Neal then Mayor of
-        Springfield appointed Mr. Williams to serve as Vice-Chairman of the
-        Board of Fire Commissioners. Governor Michael Dukakis appointed Mr.
-        Williams to serve as Vice-Chairman within the Division of Insurance for
-        the Auto Damage Licensing Board.
-      </p>
-      <p>
-        In February 2008, he was appointed by Governor Deval Patrick to the
-        Board of Directors of the Massachusetts Bay Transit Authority (MBTA).
-        Mr. Williams was elected to the Board of Directors of the Co-Operative
-        Central Bank Board of Directors and in January 2009 he was appointed to
-        the Massachusetts Bay Transportation Authority (MBTA) Retirement Fund
-        Board of Trustees. In January 2013, he was elected to the position of
-        President of the Association of Executives of the National Urban League
-        (AOE) and also appointed as a trustee to the National Urban League Board
-        of Directors.
-      </p>
-      <p>
-        A highly respected advocate and requested speaker on issues of
-        importance pertaining to the challenges faced by communities of color
-        and low-income families, Mr. Williams was a contributing member to
-        Boston’s successful effort to win the 2004 Democratic National
-        Convention. For his contributions within the Black community and the
-        City of Boston, Mayor Thomas Menino presented him with the 2003
-        Community Service Award. Mr. Williams received the 2005 Paul Parks
-        Veteran’s Community Service Award. Mayor Thomas Menino appointed him as
-        Chairman of the Roxbury Strategic Master Plan Oversight Committee
-        (RSMPOC) in 2005. In 2007, Mr. Williams was inducted into the Human
-        Resources Alliance for African Americans (HRAAA) Hall of Fame. In 2011,
-        the Urban League of Eastern Massachusetts hosted the National Urban
-        League’s Annual Conference in Boston after a 35 year absence. This event
-        has paved the way for additional conferences of color to come to Boston
-        as their host city.
-      </p>
-      <p>
-        A Gary, Indiana native, Mr. Williams earned his Master’s degree in
-        Education from Boston University after completing his undergraduate
-        degree at American International College in Springfield, MA.
-      </p>
-      <p>
-        Darnell Williams and his wife Colleen, reside in Roxbury with their
-        daughter, Imani.
-      </p>
+    <section className="container">
+      <h1 className="ulem-text-primary mt-4 text-center">Leadership</h1>
+      <div className="row">
+        <div className="col-md-4">
+          <figure class="figure">
+            <img
+              src={darnel}
+              alt="Darnell"
+              className="figure-img img-fluid rounded"
+            />
+            <figcaption>Darnell Williams</figcaption>
+          </figure>
+        </div>
+        <div className="col-md-8">
+          <p>
+            The history of the Boston Urban League now known as the Urban League
+            of Eastern Massachusetts from 1919 to 2019 with excerpts from our
+            historical archives, newspaper articles, surveys, and Urban League
+            reports:
+          </p>
+          <p>
+            The opening phrase used throughout our history at the local and
+            national level was the same; “Not Alms, But Opportunity.” This
+            phrase summarizes our philosophy and attitude towards what we
+            wanted, as citizens, in this society and how desperately we were
+            willing to work hard for it and strive for its achievement and
+            fulfillment. During our 20th anniversary report in 1939, those
+            efforts painted a world picture of 20 years on the inter-racial
+            activity in Boston by a group of forward-thinking white and colored
+            citizens. The Boston Urban League celebrated 20 years of service
+            with a deep consciousness of the many aims and ideas that have not
+            been attained, yet it holds its faith in the efficacy of
+            inter-racial cooperation as the key to this most perplexing race
+            problem.
+          </p>
+          <p>
+            In the first Annual report, Matthew W. Bullock had the following to
+            say: “Though Negroes have been living in and around Boston since
+            1632, there is probably no large city in the country where they find
+            it so difficult to secure employment that will pay a living wage.”
+            This statement was true in 1939, and it still resonates with
+            repercussions in 2019. The lack of meaningful diversity and
+            inclusion in various industries such as IT, Bio-Tech, Hospitality,
+            Health Care, Higher education persist. We are still at a woefully
+            lacking level of representation on the corporate board of directors,
+            newsrooms, philanthropic circles, and real estate development teams.
+          </p>
+          <p>
+            Have we made progress in this country? Yes. Things are somewhat more
+            covert rather than terrorizing overt acts. However, these changes
+            have not been at lightning speed. It has been a painstakingly
+            stubborn fight to create a level playing field.
+          </p>
+          <p>
+            In spite of these notable realities, however, there have been many
+            corporate partners, individual donors, foundation officers, federal,
+            state, and local supporters who have vigilantly worked with us over
+            the years to combat these ongoing challenges. They are very much a
+            vital and critical part of this historical journey. We are what we
+            have become because of your progressive input and support. Just as
+            crucial to this acknowledgment, is the time, talent, and treasures
+            of the Board of Directors, the staff of the League, the Urban League
+            Guild members and the Urban League Young Professionals Network. When
+            I reflect on the overall contributions that all of you have made
+            over these 100 years none of our accomplishments would have been
+            made possible, without you.
+          </p>
+          <p>
+            At this centennial milestone, our work is not without hope,
+            inspiration, and encouragement. Our presence in Boston has made a
+            significant difference. We see firsthand the impact on individuals
+            and their families. Their testimonies reinforce our need to stay in
+            the struggle and keep our focus and maintain our determination. As a
+            strong Affiliate, it does not matter the kind of problem or
+            challenges we face or the difficulties presented or the pain that
+            accompanies the specifics when they come thru the door. We know deep
+            down something has to be done. We can, and we must do better because
+            we are the last stop on this railroad of change.
+          </p>
+          <p>
+            For ten decades, this is what we have collectively done. So we will
+            continue to do this work until there is no need for dealing with
+            racial injustice, lack of employment opportunities and persistent
+            economic disparities. When that parity happens America, we will
+            truly live up to the Constitutional promises with the boldness of a
+            true bi-racial democracy.{" "}
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
