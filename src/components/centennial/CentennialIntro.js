2a6a43cf5f95946bb494c92ddd30c1920b7750d9
Shiyu Wang <shiyu3169@gmail.com>
2019-03-26 11:39:06 -0400

    banners and centennial intro text fix

--- src/components/centennial/CentennialIntro.js
+++ src/components/centennial/CentennialIntro.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import centennial from "../../img/logo.PNG";
 import { Link } from "react-router-dom";
+import centennialForm from "../../download/Centennial_SponsorshipOpportunities+ReplyForm-toFill.pdf";
 export default class CentennialIntro extends Component {
   render() {
     return (
       <div className="container">
-        <h1 className="ulem-text-primary mt-4">100 YEARS IN THE MAKING</h1>
         <img
           src={centennial}
           alt="centennial"
           className="img-fluid cursor center"
         />
-
-        <section>
+        <h1 className="ulem-text-primary mt-4">100 YEARS IN THE MAKING</h1>
+        <section className="mt-5">
           <h3>
             THE URBAN LEAGUE OF EASTERN MA IS CELEBRATING 100 YEARS OF SERVING
             METROPOLITIAN BOSTON
@@ -20,18 +20,23 @@ export default class CentennialIntro extends Component {
           <p>
             One hundred years ago, local business and civic leaders of diverse
             backgrounds and ethnicity created what was known as the Boston Urban
-            League. In 1919, the League brought together people that were doing
-            similar work to share in one large appeal to help people move from
-            dependency to self-sufficiency. While our role in the community has
-            not changed throughout the years, we continue to bring together the
-            resources to thousands of people and families seeking to live out
-            the American Dream of life, liberty and the pursuit of happiness.
-            This historic centennial offers an unparalleled opportunity to
-            commemorate a milestone of the civil rights movement and to explore
-            its relevance to the issues of diversity and inclusion today.
+            League. In 1919, the League brought together individuals that were
+            doing similar work to share in one large appeal to help individuals
+            move from dependency to self-sufficiency. While our role in the
+            community has not changed throughout the years, we continue to bring
+            together the resources to thousands of individuals and families
+            seeking to live out the American Dream of life, liberty and the
+            pursuit of happiness. This historic centennial offers an
+            unparalleled opportunity to commemorate a milestone of the civil
+            rights movement and to explore its relevance to the issues of
+            diversity and inclusion today.
           </p>
+          <p>For more information download the sponsorship packet below:</p>
+          <a href={centennialForm} download>
+            Download
+          </a>
         </section>
-        <section>
+        <section className="mt-5">
           <h3>OUR HISTORY</h3>
           <p>
             A lot has happened at Urban League of Eastern MA and in our
@@ -40,7 +45,7 @@ export default class CentennialIntro extends Component {
             we’ve accomplished together.
           </p>
         </section>
-        <section>
+        <section class="mt-5">
           <h3>CELEBRATE WITH US</h3>
           <p>
             We have a lot of reasons to celebrate this year!{" "}
