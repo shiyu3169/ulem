0e96c4bbd357c8a3268bc41660d92f3d4a16d126
Shiyu Wang <shiyu3169@gmail.com>
2019-04-10 15:38:39 -0400

    licence and api

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Latest updates
-- Deleted “opportunities”
+- Fix email sending
 
 ## Available Scripts
 
--- server/services/contact.service.js
+++ server/services/contact.service.js
@@ -1,14 +1,14 @@
 module.exports = function(app) {
   // Mailgun config
-  var API_KEY = "...";
-  var DOMAIN = "...";
+  var API_KEY = process.env.API_KEY || "...";
+  var DOMAIN = process.env.DOMAIN || "...";
   var mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
   // Send a contact email via mailgun
   app.post("/api/contact", (req, res) => {
     const data = req.body;
     const message = {
       from: `${data.name} <${data.email}>`,
-      to: "shiyu3169@gmail.com",
+      to: "swang@ulem.org",
       subject: data.subject,
       text: data.content
     };
