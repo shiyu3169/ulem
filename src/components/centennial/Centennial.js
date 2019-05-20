ba6febe46ce102bf30c89fc71a962e2814108b9f
Shiyu Wang <shiyu3169@gmail.com>
2019-05-20 10:23:01 -0400

    add calendar image to centennial page

--- src/components/centennial/Centennial.js
+++ src/components/centennial/Centennial.js
@@ -1,16 +1,27 @@
-import React, { Component } from "react";
-import Milestone from "../about/Milestone";
-import centennial from "../../img/logo.PNG";
+import React, { Component } from 'react';
+import Milestone from '../about/Milestone';
+import centennial from '../../img/logo.PNG';
+import calendar from '../../img/calendar.jpg';
+
 export default class Centennial extends Component {
   render() {
     return (
-      <div className="container">
+      <div className='container-fluid'>
         <img
           src={centennial}
-          alt="centennial"
-          className="img-fluid cursor center"
+          alt='centennial'
+          className='img-fluid cursor center'
         />
-        <Milestone />
+        <div className='row'>
+          <div className='col-lg-2'>
+            <img src={calendar} alt='Calender' />
+          </div>
+          <div className='col-lg-1' />
+          <div className='col-lg-7'>
+            <Milestone />
+          </div>
+          <div className='col-lg-2' />
+        </div>
       </div>
     );
   }
