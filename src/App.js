d0096dc6845ef6486c5520798129cbff96a1a385
Shiyu Wang <shiyu3169@gmail.com>
2019-06-21 13:28:06 -0400

    client side payment form

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./app.css";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
 
-import Home from "./components/Home/Home";
-import About from "./components/about/About";
-import Navbar from "./components/layout/Navbar";
-import Program from "./components/program/Program";
-import Events from "./components/event/Events";
-import Donate from "./components/support/Donate";
-import Sponsor from "./components/support/Sponsor";
-import Impact from "./components/about/Impact";
-import Auxiliaries from "./components/support/Auxiliaries";
-import ScrollToTop from "./components/layout/ScrollToTop";
-import Footer from "./components/layout/Footer";
-import Centennial from "./components/centennial/Centennial";
-import Contact from "./components/about/Contact";
-import Interest from "./components/program/Interest";
-import Thank from "./components/layout/Thank";
-import Login from "./components/auth/Login";
-import Admin from "./components/auth/Admin";
-import Marathon from "./components/marathon/Marathon";
-import Membership from "./components/membership/Membership";
-import CentennialMembership from "./components/centennial/CentennialMembership";
-import Leadership from "./components/about/Leadership";
-import Board from "./components/about/Board";
-import HowToDonate from "./components/support/HowToDonate";
-import CentennialIntro from "./components/centennial/CentennialIntro";
-import Staff from "./components/about/Staff";
+import Home from './components/Home/Home';
+import About from './components/about/About';
+import Navbar from './components/layout/Navbar';
+import Program from './components/program/Program';
+import Events from './components/event/Events';
+import Donate from './components/support/Donate';
+import Sponsor from './components/support/Sponsor';
+import Impact from './components/about/Impact';
+import Auxiliaries from './components/support/Auxiliaries';
+import ScrollToTop from './components/layout/ScrollToTop';
+import Footer from './components/layout/Footer';
+import Centennial from './components/centennial/Centennial';
+import Contact from './components/about/Contact';
+import Interest from './components/program/Interest';
+import Thank from './components/layout/Thank';
+import Login from './components/auth/Login';
+import Admin from './components/auth/Admin';
+import Marathon from './components/marathon/Marathon';
+import Membership from './components/membership/Membership';
+import CentennialMembership from './components/centennial/CentennialMembership';
+import Leadership from './components/about/Leadership';
+import Board from './components/about/Board';
+import HowToDonate from './components/support/HowToDonate';
+import CentennialIntro from './components/centennial/CentennialIntro';
+import Staff from './components/about/Staff';
+import Payment from './components/layout/Payment';
 
 class App extends Component {
   render() {
@@ -35,30 +36,31 @@ class App extends Component {
       <Router>
         <ScrollToTop>
           <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/leadership" component={Leadership} />
-          <Route exact path="/board" component={Board} />
-          <Route exact path="/staff" component={Staff} />
-          <Route exact path="/program" component={Program} />
-          <Route exact path="/events" component={Events} />
-          <Route exact path="/donate" component={Donate} />
-          <Route exact path="/sponsor" component={Sponsor} />
-          <Route exact path="/impact" component={Impact} />
-          <Route exact path="/auxiliaries" component={Auxiliaries} />
-          <Route exact path="/centennial" component={Centennial} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/interest" component={Interest} />
-          <Route exact path="/thank" component={Thank} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/admin" component={Admin} />
-          <Route exact path="/marathon" component={Marathon} />
-          <Route exact path="/howToDonate" component={HowToDonate} />
-          <Route exact path="/centennialIntro" component={CentennialIntro} />
-          <Route exact path="/membership" component={Membership} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/leadership' component={Leadership} />
+          <Route exact path='/board' component={Board} />
+          <Route exact path='/staff' component={Staff} />
+          <Route exact path='/program' component={Program} />
+          <Route exact path='/events' component={Events} />
+          <Route exact path='/donate' component={Donate} />
+          <Route exact path='/sponsor' component={Sponsor} />
+          <Route exact path='/impact' component={Impact} />
+          <Route exact path='/auxiliaries' component={Auxiliaries} />
+          <Route exact path='/centennial' component={Centennial} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/interest' component={Interest} />
+          <Route exact path='/thank' component={Thank} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/admin' component={Admin} />
+          <Route exact path='/marathon' component={Marathon} />
+          <Route exact path='/howToDonate' component={HowToDonate} />
+          <Route exact path='/centennialIntro' component={CentennialIntro} />
+          <Route exact path='/membership' component={Membership} />
+          <Route exact path='/payment' component={Payment} />
           <Route
             exact
-            path="/centennialMembership"
+            path='/centennialMembership'
             component={CentennialMembership}
           />
           <Footer />
---
+++ src/components/layout/Payment.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import SquarePaymentForm, {
+  CreditCardNumberInput,
+  CreditCardExpirationDateInput,
+  CreditCardPostalCodeInput,
+  CreditCardCVVInput,
+  CreditCardSubmitButton
+} from 'react-square-payment-form';
+import 'react-square-payment-form/lib/default.css';
+
+export default class Payment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errorMessages: []
+    };
+  }
+
+  cardNonceResponseReceived = (errors, nonce, cardData) => {
+    if (errors) {
+      this.setState({ errorMessages: errors.map(error => error.message) });
+      return;
+    }
+    this.setState({ errorMessages: [] });
+    alert('nonce created: ' + nonce);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Payment Page</h1>
+        <SquarePaymentForm
+          applicationId='sandbox-sq0idp-OfqC0M7Zle4IW-veWzvVlQ'
+          locationId='CBASEMKYs1pNXgCZcvdmuOUXd_MgAQ'
+          cardNonceResponseReceived={this.cardNonceResponseReceived}
+        >
+          <fieldset className='sq-fieldset'>
+            <CreditCardNumberInput />
+            <div className='sq-form-third'>
+              <CreditCardExpirationDateInput />
+            </div>
+
+            <div className='sq-form-third'>
+              <CreditCardPostalCodeInput />
+            </div>
+
+            <div className='sq-form-third'>
+              <CreditCardCVVInput />
+            </div>
+          </fieldset>
+
+          <CreditCardSubmitButton>Pay $1.00</CreditCardSubmitButton>
+        </SquarePaymentForm>
+        <div className='sq-error-message'>
+          {this.state.errorMessages.map(errorMessage => (
+            <li key={`sq-error-${errorMessage}`}>{errorMessage}</li>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
