fbe8bab737f6422c661b418b70bf6029f9d81b59
Shiyu Wang <shiyu3169@gmail.com>
2020-02-06 10:35:16 -0500

    replace marathon banners on top slides and marathon page

--- src/components/Home/TopSlides.js
+++ src/components/Home/TopSlides.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import msimbo from "../../img/MSIMBOFlyer.png";
-import digiul from "../../img/DigiUL.png";
-import ulemBuilding from "../../img/ulemBuilding.JPG";
-import marathonBanner from "../../img/ULEM-MarathonBanner2020.png";
-import banner100 from "../../img/Banner100.png";
-import jobBanner from "../../img/jobBanner.png";
+import React, { Component } from 'react';
+import msimbo from '../../img/MSIMBOFlyer.png';
+import digiul from '../../img/DigiUL.png';
+import ulemBuilding from '../../img/ulemBuilding.JPG';
+import marathonBanner from '../../img/ULEM-MarathonBanner20202_A.jpg';
+import banner100 from '../../img/Banner100.png';
+import jobBanner from '../../img/jobBanner.png';
 // import logoBanner from "../../img/sponser_banner.jpg";
 
-import $ from "jquery";
-import "popper.js";
-import "bootstrap";
-import TopSlide from "./TopSlide";
+import $ from 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import TopSlide from './TopSlide';
 
 export default class TopSlides extends Component {
   componentDidMount() {
-    $(".top-carousel").carousel({
+    $('.top-carousel').carousel({
       interval: 2400
     });
   }
@@ -22,67 +22,67 @@ export default class TopSlides extends Component {
   render() {
     return (
       <div
-        id="carouselExampleControls"
-        className="carousel slide top-carousel mb-3"
-        data-ride="carousel"
+        id='carouselExampleControls'
+        className='carousel slide top-carousel mb-3'
+        data-ride='carousel'
       >
-        <ol className="carousel-indicators">
+        <ol className='carousel-indicators'>
           <li
-            data-target="#carouselExampleControls"
-            data-slide-to="0"
-            className="active"
+            data-target='#carouselExampleControls'
+            data-slide-to='0'
+            className='active'
           />
-          <li data-target="#carouselExampleControls" data-slide-to="1" />
-          <li data-target="#carouselExampleControls" data-slide-to="2" />
-          <li data-target="#carouselExampleControls" data-slide-to="3" />
-          <li data-target="#carouselExampleControls" data-slide-to="4" />
-          <li data-target="#carouselExampleControls" data-slide-to="5" />
-          <li data-target="#carouselExampleControls" data-slide-to="6" />
+          <li data-target='#carouselExampleControls' data-slide-to='1' />
+          <li data-target='#carouselExampleControls' data-slide-to='2' />
+          <li data-target='#carouselExampleControls' data-slide-to='3' />
+          <li data-target='#carouselExampleControls' data-slide-to='4' />
+          <li data-target='#carouselExampleControls' data-slide-to='5' />
+          <li data-target='#carouselExampleControls' data-slide-to='6' />
         </ol>
-        <div className="carousel-inner">
+        <div className='carousel-inner'>
           <TopSlide
             img={ulemBuilding}
-            to="/about"
-            active="active"
-            h1=" Empowering Communities. Changing Lives."
-            h3="From Experiment To Experience To Exposure"
-            x="center"
-            y="top"
+            to='/about'
+            active='active'
+            h1=' Empowering Communities. Changing Lives.'
+            h3='From Experiment To Experience To Exposure'
+            x='center'
+            y='top'
           />
           <TopSlide
             img={banner100}
-            to="/centennialIntro"
-            x="center"
-            y="center"
+            to='/centennialIntro'
+            x='center'
+            y='center'
           />
           {/* <TopSlide img={logoBanner} to="/sponsor" x="center" y="center" /> */}
-          <TopSlide img={digiul} to="/program" x="center" y="top" />
-          <TopSlide img={msimbo} to="/program" x="center" y="top" />
-          <TopSlide img={marathonBanner} to="/marathon" x="center" y="center" />
+          <TopSlide img={digiul} to='/program' x='center' y='top' />
+          <TopSlide img={msimbo} to='/program' x='center' y='top' />
+          <TopSlide img={marathonBanner} to='/marathon' x='center' y='center' />
           <TopSlide
             img={jobBanner}
-            href="https://www.jobcase.com/ulem"
-            x="center"
-            y="bottom"
+            href='https://www.jobcase.com/ulem'
+            x='center'
+            y='bottom'
           />
         </div>
         <a
-          className="carousel-control-prev"
-          href="#carouselExampleControls"
-          role="button"
-          data-slide="prev"
+          className='carousel-control-prev'
+          href='#carouselExampleControls'
+          role='button'
+          data-slide='prev'
         >
-          <span className="carousel-control-prev-icon" aria-hidden="true" />
-          <span className="sr-only">Previous</span>
+          <span className='carousel-control-prev-icon' aria-hidden='true' />
+          <span className='sr-only'>Previous</span>
         </a>
         <a
-          className="carousel-control-next"
-          href="#carouselExampleControls"
-          role="button"
-          data-slide="next"
+          className='carousel-control-next'
+          href='#carouselExampleControls'
+          role='button'
+          data-slide='next'
         >
-          <span className="carousel-control-next-icon" aria-hidden="true" />
-          <span className="sr-only">Next</span>
+          <span className='carousel-control-next-icon' aria-hidden='true' />
+          <span className='sr-only'>Next</span>
         </a>
       </div>
     );
--- src/components/marathon/Marathon.js
+++ src/components/marathon/Marathon.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import MarathonNav from "./MarathonNav";
-import MarathonAbout from "./MarathonAbout";
-import MarathonBanner from "../../img/ULEM-MarathonBanner20202.png";
+import React, { Component } from 'react';
+import MarathonNav from './MarathonNav';
+import MarathonAbout from './MarathonAbout';
+import MarathonBanner from '../../img/ULEM-MarathonBanner20202_A.jpg';
 export default class Marathon extends Component {
   render() {
     return (
@@ -9,11 +9,11 @@ export default class Marathon extends Component {
         <MarathonNav />
         <img
           src={MarathonBanner}
-          alt="Marathon"
-          className="img-fluid"
-          width="100%"
+          alt='Marathon'
+          className='img-fluid'
+          width='100%'
         />
-        <div className="container">
+        <div className='container'>
           <MarathonAbout />
         </div>
       </div>
