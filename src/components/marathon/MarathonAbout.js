735df95379fccbe9cb7bc89184deaa3750c3b389
Shiyu Wang <shiyu3169@gmail.com>
2020-03-20 14:27:38 -0700

    change marathon text

--- src/components/marathon/MarathonAbout.js
+++ src/components/marathon/MarathonAbout.js
@@ -6,27 +6,24 @@ export default function MarathonAbout() {
     <section id='about'>
       <h1 className='font-red mt-4'>About Us</h1>
       <p>
-        Run for The Urban League of Eastern Massachusetts Team in the 2020
-        Boston Marathon(r) on Monday, April 20, 2020
+        The 2020 Boston Marathon has been postponed until Monday, September 14,
+        2020.
       </p>
       <img src={ulem} alt='ulem' className='img-fluid' />
 
       <p className='mt-3'>
-        We are currently accepting applications for runners on a rolling basis.
-        Applicants that require a bib will be asked to raise a minimum of $8,500
-        for the foundation. Qualified runners that already have a bib are
-        welcome to join our team and are asked to raise a minimum of $2,000.
+        <strong>We are no longer accepting applications for runners.</strong>{' '}
+        Applicants that require a bib will be asked to raise a minimum of
+        $10,000 for the foundation. Qualified runners that already have a bib
+        are welcome to join our team and are asked to raise a minimum of $2,000.
+      </p>
+      <p>
+        We appreciate all donations to our Marathon Team. Fund received support
+        the services and programs provided by The Urban League of Eastern
+        Massachusetts.
       </p>
 
       <section id='sponsors'>
-        <a
-          href='https://charityteamsruns.wufoo.com/forms/z9j4g8b0kmy29y/'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='btn btn-block ulem-btn-primary btn-lg'
-        >
-          Fill out the application here
-        </a>
         <a
           href='https://charity.gofundme.com/o/en/campaign/urbanleague2020'
           target='_blank'
@@ -35,6 +32,15 @@ export default function MarathonAbout() {
         >
           Donate Here
         </a>
+        <a
+          href='https://charityteamsruns.wufoo.com/forms/z9j4g8b0kmy29y/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='btn btn-block ulem-btn-primary btn-lg'
+        >
+          Fill out the application here
+        </a>
+
         <h1 className='font-red mt-4'>Sponsors</h1>
         <img
           src={charity}
@@ -105,20 +111,20 @@ export default function MarathonAbout() {
           </li>
         </ul>
         <a
-          href='https://charityteamsruns.wufoo.com/forms/z9j4g8b0kmy29y/'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='btn btn-block ulem-btn-primary btn-lg'
-        >
-          Fill out the application here
-        </a>
-        <a
           href='https://charity.gofundme.com/o/en/campaign/urbanleague2020'
           target='_blank'
           rel='noopener noreferrer'
           className='btn btn-block btn-dark btn-lg'
         >
           Donate Here
+        </a>
+        <a
+          href='https://charityteamsruns.wufoo.com/forms/z9j4g8b0kmy29y/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='btn btn-block ulem-btn-primary btn-lg'
+        >
+          Fill out the application here
         </a>
       </section>
     </section>
